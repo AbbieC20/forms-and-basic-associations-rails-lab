4f3e3c79a903f246bd4c8b97678394f6b6a84f04
Abbie Cornelius <abbyc@sky.com>
2021-06-29 19:13:25 +0100

    commit tree 94a52b626513d00908c7df1f980ac9b3c279de2b

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,27 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  has_many :notes
+  belongs_to :artist
+  belongs_to :genre
+
+  def artist_name=(artist_name)
+    self.artist = Artist.find_or_create_by(name: artist_name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_contents=(note_contents)
+    note_contents.each do |content|
+      if content.strip != ""
+        note = Note.find_or_create_by(content: content)
+        self.notes << note
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map { |note| note.content }
+  end
+
 end
